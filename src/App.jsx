b7819f5d3be0514db3a248bc5946ccae38ebbbdd
TanKittipat <kittipat12tan@gmail.com>
2024-06-24 13:46:46 +0700

    Create components for title and search

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import Card from "./Components/Card";
+import Search from "./Components/Search";
+import Title from "./Components/Title";
 
 export default function App() {
   return (
     <div class="container mx-auto my-5">
-      <h1 className="text-5xl font-semibold text-center my-7">
-        Grab Restaurant
-      </h1>
-      <div id="search">
-        <label className="input input-bordered flex items-center gap-2 my-5 mx-3">
-          <input type="text" className="grow" placeholder="Search" />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </label>
-      </div>
+      <Title />
+      <Search />
       <div className="flex flex-wrap justify-center gap-4" id="cards">
         <Card
           img={
---
+++ src/Components/Title.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <h1 className="text-5xl font-semibold text-center my-7">Grab Restaurant</h1>
+  );
+};
+
+export default Title;
