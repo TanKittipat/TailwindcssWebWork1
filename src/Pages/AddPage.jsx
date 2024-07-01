3ed12aba05c7caaf32ccca7ac2b85f24a85e009b
TanKittipat <kittipat12tan@gmail.com>
2024-07-01 13:19:19 +0700

    Create add page

--- src/Pages/AddPage.jsx
+++ src/Pages/AddPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Form from "../Components/Form";
+import Navbar from "../Components/Navbar";
 
 const AddPage = () => {
-  return <div className="container mx-auto my-5">AddPage</div>;
+  return (
+    <div className="container flex flex-row flex-wrap items-center justify-center mx-auto">
+      <Navbar />
+      <Form />
+    </div>
+  );
 };
 
 export default AddPage;
